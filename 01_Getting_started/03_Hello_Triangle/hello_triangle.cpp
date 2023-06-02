f33aa31bd7a74df7a6a4586ec06fd2701cb6b8cc
Delbert <code@delbert.me>
2023-06-03 00:15:11 +0800

    After Bind, we can change buffer and vertexArray
    Signed-off-by: Delbert <code@delbert.me>

--- 01_Getting_started/03_Hello_Triangle/hello_triangle.cpp
+++ 01_Getting_started/03_Hello_Triangle/hello_triangle.cpp
@@ -119,9 +119,11 @@ int main()
     glBindVertexArray(VAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float ), (void*)0);
     glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+//    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
