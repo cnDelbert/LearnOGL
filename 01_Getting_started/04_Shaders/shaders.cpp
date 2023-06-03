1924a4af4f272ef3868f280fbea39cd7cdf44259
Delbert <code@delbert.me>
2023-06-03 15:14:48 +0800

    output OpenGL version and vender
    Signed-off-by: Delbert <code@delbert.me>

--- 01_Getting_started/04_Shaders/shaders.cpp
+++ 01_Getting_started/04_Shaders/shaders.cpp
@@ -13,7 +13,8 @@ int main()
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     int mAttributes;
     glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &mAttributes);
-
+    std::cout << "OpenGL version is " << glGetString(GL_VERSION) << "\nGL vender is " << glGetString(GL_VENDOR)
+              << std::endl;
     std::cout << "Max number of vertex attributes support is " << mAttributes << std::endl;
     return 0;
 }
