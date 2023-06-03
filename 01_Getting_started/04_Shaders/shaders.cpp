5ccd06ce17b9490884765c98eaeb9d489f8254f5
Delbert <code@delbert.me>
2023-06-03 10:39:58 +0800

    Check number of Vertex Attributes support.
    Signed-off-by: Delbert <code@delbert.me>

---
+++ 01_Getting_started/04_Shaders/shaders.cpp
@@ -0,0 +1,19 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+//    glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Learning OpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    int mAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &mAttributes);
+
+    std::cout << "Max number of vertex attributes support is " << mAttributes << std::endl;
+    return 0;
+}
