6ec02071446bd3a06a573bfcff90aeb4a486760c
Delbert <code@delbert.me>
2023-06-03 16:36:52 +0800

    More attributes to shaders.
    Signed-off-by: Delbert <code@delbert.me>

--- 01_Getting_started/04_Shaders/shaders.cpp
+++ 01_Getting_started/04_Shaders/shaders.cpp
@@ -9,15 +9,17 @@ constexpr int WIN_WIDTH = 800;
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
-                                 "\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 ourColor;\n"
                                  "void main()\n"
                                  "{\n"
-                                 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "    gl_Position = vec4(aPos, 1.0);\n"
+                                 "    ourColor = aColor;\n"
                                  "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
-                                   "uniform vec3 ourColor;\n"
+                                   "in vec3 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
                                    "    FragColor = vec4(ourColor, 1.0f);\n"
@@ -69,9 +71,10 @@ int main()
     }
 
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
+            // 位置              // 颜色
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // 右下
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // 左下
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // 顶部
     };
 
     int success;
@@ -125,8 +128,10 @@ int main()
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float ), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float ), (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float ), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
 // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 //    glBindBuffer(GL_ARRAY_BUFFER, 0);
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
@@ -142,7 +147,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // 状态设置函数
         glClear(GL_COLOR_BUFFER_BIT);   // 状态使用函数
 
-        changeUniformValue(shaderProgram);
+//        changeUniformValue(shaderProgram);
+        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0);
