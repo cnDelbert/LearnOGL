a80e16072d0b75b6dc42e990c5ab6aa4604edbd4
Delbert <code@delbert.me>
2023-06-14 21:53:53 +0800

    correct DEPTH_TEST
    Signed-off-by: Delbert <code@delbert.me>

--- 01_Getting_started/07_Coordinate_Systems/coordinate_systems.cpp
+++ 01_Getting_started/07_Coordinate_Systems/coordinate_systems.cpp
@@ -170,9 +170,10 @@ int main()
         // Outside keyboard input process
         processInput(window);
 
+        glEnable(GL_DEPTH_TEST);
         // Rendering
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // 状态设置函数
-        glClear(GL_COLOR_BUFFER_BIT);   // 状态使用函数
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // 状态使用函数
 
         glBindTexture(GL_TEXTURE_2D, texture);
 
